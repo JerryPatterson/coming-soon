12730888f72e02f9432fcdd8a2147a3f4a554663
Intriligator <daniel@peekskillwebdesign.com>
2016-06-13 15:56:56 -0400

    fixed fade menu. fixed hr bug. added cursor class

--- js/menu-overlay.js
+++ js/menu-overlay.js
@@ -1,4 +1,4 @@
-jQuery.fn.fademenu = function(target) {
+jQuery.fn.fadeMenu = function(target) {
 	jQuery(target).hide();
 	jQuery(target).wrapInner('<div class="mobile-menu-table"></div>')
   jQuery(this).on('click', function(e){
