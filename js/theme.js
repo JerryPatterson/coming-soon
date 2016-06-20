8fd1884de6ea517adff0c989e934c7977ef6d01d
JerryPatterson <jerry@peekskillwebdesign.com>
2016-06-20 09:15:41 -0400

    final update to theme js

--- js/theme.js
+++ js/theme.js
@@ -1,10 +1,25 @@
 jQuery(document).ready(function(){
 
+//---Headroom
+var header=document.querySelector("#header");
+new Headroom(header,{
+tolerance:{down:2,up:5},
+offset:100,
+classes:{
+  initial:"slide",
+  pinned:"slide--reset",
+  unpinned:"slide--up"}
+}).init();
+
+//---Fade Menu
+jQuery("#mobile-menu-btn").fadeMenu('#mobile-menu');
+
 // Sidr
 jQuery("#right-menu").sidr({
   name:"sidr-right",
   side:"right"
-});   
+}); 
+
 //  Back to top
 var offset = 500;
 var duration = 300;
@@ -22,11 +37,13 @@ jQuery('.back-to-top').click(function(event) {
   jQuery('html, body').animate({scrollTop: 0}, duration);
   return false;
 });
+
 // Accordian 
 jQuery('._accordian-content').hide();
 jQuery('._accordian-title').on('click', toggleAccordian);
 
-});//End document ready
+});
+//End document ready
 
 // Functions
 
